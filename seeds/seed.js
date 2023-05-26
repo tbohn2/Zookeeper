3df3515de9e498cb0461baba5d52bdb5f0aa6ba5
cgonz <christoph.gonz@gmail.com>
2023-05-25 21:05:01 -0700

    starting the seeds

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,9 @@
-const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const sequelize = require("../config/connection");
+const { User, Pokemon } = require("../models");
 
-const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const userData = require("./userData.json");
+
+let pokeData;
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,12 +13,14 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
-      user_id: users[Math.floor(Math.random() * users.length)].id,
-    });
-  }
+  // for (const project of projectData) {
+  //   await Project.create({
+  //     ...project,
+  //     user_id: users[Math.floor(Math.random() * users.length)].id,
+  //   });
+  // }
+
+  const pokemon = await Pokemon.bulkCreate();
 
   process.exit(0);
 };
