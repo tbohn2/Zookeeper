71e88e48985c47680f3d2e124977b895f4af9510
tbohn2 <tanner.bohn@yahoo.com>
2023-06-01 21:31:53 -0700

    Added dashboard route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,10 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const projectRoutes = require("./projectRoutes");
 const builderRoutes = require("./builderRoutes");
+const dashboardRoutes = require("./dashboardRoutes");
 
 router.use("/users", userRoutes);
 router.use("/projects", projectRoutes);
 router.use("/builder", builderRoutes);
+router.use("/dashboard", dashboardRoutes);
 module.exports = router;
