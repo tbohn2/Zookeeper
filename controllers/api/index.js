94d13bb69b77ffeb4442a2f514ab7a0b12deb3de
cgonz <christoph.gonz@gmail.com>
2023-06-01 20:14:26 -0700

    Added new partials+routes for pokemon and species

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -2,8 +2,12 @@ const router = require("express").Router();
 const userRoutes = require("./userRoutes");
 const projectRoutes = require("./projectRoutes");
 const builderRoutes = require("./builderRoutes");
+const pokemonRoutes = require("./pokemonRoutes");
+const speciesRoutes = require("./speciesRoutes");
 
 router.use("/users", userRoutes);
 router.use("/projects", projectRoutes);
 router.use("/builder", builderRoutes);
+router.use("/pokemon", pokemonRoutes);
+router.use("/species", speciesRoutes);
 module.exports = router;
