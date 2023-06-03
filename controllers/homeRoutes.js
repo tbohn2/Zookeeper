34e93fa0293feba797b0a14856135b66445ec805
jorgemunoz7 <jamunoz9@asu.edu>
2023-06-03 09:48:46 -0700

    Routes for Homepage
    Routes for Homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,7 +2,25 @@ const router = require('express').Router();
 const { Pokehome, Pokemon, Species } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/dashboard', async (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    res.render('homepage', {
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/login', async (req, res) => {
+  try {
+    res.render('login', {
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/dashboard', withAuth, async (req, res) => {
   try {
     // const user_id = req.session.user_id
     const user_id = 1
