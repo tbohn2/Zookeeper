4d56a368e1d57a1e8152d63ec048bd831fbb2fa2
Dylan Kazunaga <dylankazunaga@Dylans-MacBook-Air.local>
2023-06-04 17:54:45 -0700

    updated login.js

--- public/js/login.js
+++ public/js/login.js
@@ -6,43 +6,19 @@ function setFormMessage(formElement, type, message) {
   messageElement.classList.add(`form__message--${type}`);
 }
 
-function setInputError(inputElement, message) {
-  inputElement.classList.add("form__input--error");
-  inputElement.parentElement.querySelector(".form__input-error-message").textContent = message;
-}
-
-function clearInputError(inputElement) {
-  inputElement.classList.remove("form__input--error");
-  inputElement.parentElement.querySelector(".form__input-error-message").textContent = "";
-}
-
 document.addEventListener("DOMContentLoaded", () => {
   const loginForm = document.querySelector("#login");
   const createAccountForm = document.querySelector("#createAccount");
   const homePageForm = document.querySelector("#homePage");
-
-  document.querySelector("#linkCreateAccount").addEventListener("click", e => {
-    e.preventDefault();
-    loginForm.classList.add("form--hidden");
-    createAccountForm.classList.remove("form--hidden");
-  });
+}
 
   document.querySelector("#linkLogin").addEventListener("click", e => {
     e.preventDefault();
-    loginForm.classList.remove("form--hidden");
-    createAccountForm.classList.add("form--hidden");
-    
   });
 
   loginForm.addEventListener("submit", e => {
     e.preventDefault();
-    loginForm.classList.remove("form--hidden");
-    homePageForm.classList.add("form--hidden");
 
-    // Perform your AJAX/Fetch login
-
-    setFormMessage(loginForm, "error", "Invalid username/password combination");
-  });
 
 document.querySelectorAll(".form__input").forEach(inputElement => {
   inputElement.addEventListener("blur", e => {
