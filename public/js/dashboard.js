45431b67d496e46ef1320c628d247c48cef77cea
tbohn2 <tanner.bohn@yahoo.com>
2023-06-05 19:04:46 -0700

    Removed logout button in dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -38,18 +38,4 @@ const createZoo = async (name, description) => {
 //   });
 // });
 
-const logout = async () => {
-  const response = await fetch("/api/users/logout", {
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-  });
-
-  if (response.ok) {
-    document.location.replace("/");
-  } else {
-    alert(response.statusText);
-  }
-};
-
 document.querySelector("#createbtn").addEventListener("click", newName);
-document.querySelector("#logoutbtn").addEventListener("click", logout);
